4b02638a9dd5c21eb4706b53db915e8ecb32d540
hd_pro <gitTest_pro@hd.com>
2022-10-27 16:21:33 +0800

    add middleware ok

--- main.go
+++ main.go
@@ -2,11 +2,22 @@ package main
 
 import (
 	"goweb/framework"
+	"goweb/framework/middleware"
 	"net/http"
 )
 
 func main() {
 	core := framework.NewCore()
+	// core 中使用 use 注册中间件
+	//core.Use(
+	//	middleware.Test1(),
+	//	middleware.Test2())
+
+	// 为所有的路由都设置 Recovery 中间件
+	core.Use(middleware.Recovery())
+	//core.Use(middleware.Cost())
+	// core.Use(middleware.Timeout(1 * time.Second))
+
 	registerRouter(core)
 	server := &http.Server{
 		Handler: core,
