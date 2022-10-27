fff23be18348c19e99124471efa26780691b75ba
hd_pro <gitTest_pro@hd.com>
2022-10-27 18:18:17 +0800

    add close

--- main.go
+++ main.go
@@ -1,27 +1,47 @@
 package main
 
 import (
+	"context"
 	"goweb/framework"
 	"goweb/framework/middleware"
+	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 )
 
 func main() {
 	core := framework.NewCore()
-	// core 中使用 use 注册中间件
-	//core.Use(
-	//	middleware.Test1(),
-	//	middleware.Test2())
 
 	// 为所有的路由都设置 Recovery 中间件
 	core.Use(middleware.Recovery())
-	//core.Use(middleware.Cost())
-	// core.Use(middleware.Timeout(1 * time.Second))
+	core.Use(middleware.Cost())
 
 	registerRouter(core)
 	server := &http.Server{
 		Handler: core,
 		Addr:    ":8888",
 	}
-	server.ListenAndServe()
+
+	// 这个 goroutine 是启动服务的 goroutine
+	go func() {
+		server.ListenAndServe()
+	}()
+
+	// 当前的 goroutine 等待信号量
+	quit := make(chan os.Signal)
+	// 监控信号：SIGINT, SIGTERM, SIGQUIT
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT)
+	// 这里会阻塞当前goroutine等待信号
+	<-quit
+
+	// 调用Server.Shutdown graceful结束
+	timeoutCtx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	if err := server.Shutdown(timeoutCtx); err != nil {
+		log.Fatal("Server Shutdown:", err)
+	}
 }
