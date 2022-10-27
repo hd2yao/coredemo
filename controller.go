81c293521b33bd3ce894c44fbe67d56faecdba3e
hd_pro <gitTest_pro@hd.com>
2022-10-27 16:21:09 +0800

    add timeout middleware ok

--- controller.go
+++ controller.go
@@ -32,83 +32,25 @@ func FooControllerHandler(c *framework.Context) error {
 
 		// 新的 goroutine 结束时通过一个 finish 通道告知 父 goroutine
 		finish <- struct{}{}
-
-		// 3.监听
-		select {
-		// 监听 panic
-		case p := <-panicChan:
-			c.WriterMux().Lock()
-			defer c.WriterMux().Unlock()
-			log.Println(p)
-			c.Json(500, "panic")
-		// 监听结束事件
-		case <-finish:
-			fmt.Println("finish")
-		// 监听超时事件
-		case <-durationCtx.Done():
-			c.WriterMux().Lock()
-			defer c.WriterMux().Unlock()
-			c.Json(500, "time out")
-			c.SetHasTimeout()
-		}
 	}()
+	// 3.监听
+	select {
+	// 监听 panic
+	case p := <-panicChan:
+		c.WriterMux().Lock()
+		defer c.WriterMux().Unlock()
+		log.Println(p)
+		c.Json(500, "panic")
+	// 监听结束事件
+	case <-finish:
+		fmt.Println("finish")
+	// 监听超时事件
+	case <-durationCtx.Done():
+		c.WriterMux().Lock()
+		defer c.WriterMux().Unlock()
+		c.Json(500, "time out")
+		c.SetHasTimeout()
+	}
 
 	return nil
 }
-
-//// Foo1 控制器(未封装)
-//func Foo1(request *http.Request, response http.ResponseWriter) {
-//	obj := map[string]interface{}{
-//		"data": nil,
-//	}
-//
-//	// 设置控制器的 response 的 header 部分
-//	response.Header().Set("Content-Type", "application/json")
-//
-//	// 从请求体中获取参数
-//	foo := request.PostFormValue("foo")
-//	if foo == "" {
-//		foo = "10"
-//	}
-//	fooInt, err := strconv.Atoi(foo)
-//	if err != nil {
-//		response.WriteHeader(500)
-//		return
-//	}
-//
-//	// 构建返回结构
-//	obj["data"] = fooInt
-//	byt, err := json.Marshal(obj)
-//	if err != nil {
-//		response.WriteHeader(500)
-//		return
-//	}
-//
-//	// 构建返回状态，输出返回结构
-//	response.WriteHeader(200)
-//	response.Write(byt)
-//	return
-//}
-//
-//// Foo2 控制器(封装)
-//func Foo2(ctx *framework.Context) error {
-//	obj := map[string]interface{}{
-//		"data": nil,
-//	}
-//
-//	// 从请求体中获取参数
-//	fooInt := ctx.FormInt("foo", 10)
-//	// 构建返回结构
-//	obj["data"] = fooInt
-//	// 输出返回结构
-//	return ctx.Json(http.StatusOK, obj)
-//}
-//
-//func Foo3(ctx *framework.Context) error {
-//	rdb := redis.NewClient(&redis.Options{
-//		Addr:     "localhost:6379",
-//		Password: "", // no password set
-//		DB:       0,  // use default DB
-//	})
-//	return rdb.Set(ctx, "key", "value", 0).Err()
-//}
