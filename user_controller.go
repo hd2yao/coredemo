406a3c91beaa1c6ece0e80ca057326d2febe2871
hd_pro <gitTest_pro@hd.com>
2022-10-27 18:16:42 +0800

    update status json output

--- user_controller.go
+++ user_controller.go
@@ -1,8 +1,16 @@
 package main
 
-import "goweb/framework"
+import (
+	"goweb/framework"
+	"time"
+)
 
 func UserLoginController(c *framework.Context) error {
-	c.Json(200, "ok, UserLoginController")
+	foo, _ := c.QueryString("foo", "def")
+	// 等待10s才结束执行
+	time.Sleep(10 * time.Second)
+	// 输出结果
+	c.SetOkStatus().Json("ok, UserLoginController: " + foo)
+	return nil
 	return nil
 }
