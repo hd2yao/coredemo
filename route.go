7e9a3cafae73e4c5f80930f3dce5f8b3c5099f40
hd_pro <gitTest_pro@hd.com>
2022-10-27 16:21:43 +0800

    add middleware ok

--- route.go
+++ route.go
@@ -1,6 +1,9 @@
 package main
 
-import "goweb/framework"
+import (
+	"goweb/framework"
+	"goweb/framework/middleware"
+)
 
 /*
 	路由规则的需求：
@@ -13,7 +16,7 @@ import "goweb/framework"
 // 注册路由规则
 func registerRouter(core *framework.Core) {
 	// 需求1+2：静态路由 + HTTP方法匹配
-	core.Get("/user/login", UserLoginController)
+	core.Get("/user/login", middleware.Test3(), UserLoginController)
 
 	// 需求3：批量通用前缀
 	subjectApi := core.Group("/subject")
@@ -21,7 +24,7 @@ func registerRouter(core *framework.Core) {
 		// 动态路由
 		subjectApi.Delete("/:id", SubjectDelController)
 		subjectApi.Put("/:id", SubjectUpdateController)
-		subjectApi.Get("/:id", SubjectGetController)
+		subjectApi.Get("/:id", middleware.Test3(), SubjectGetController)
 		subjectApi.Get("/list/all", SubjectListController)
 
 		//subjectInnerApi := subjectApi.Group("/info")
